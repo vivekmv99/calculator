9068bc7aa024cb2471c1619ae57da853cf692ff2
Vivek Mv <mvvivek99@gmail.com>
2020-07-09 21:47:54 +0530

    buttons design completed

--- calcu_code.py
+++ calcu_code.py
@@ -1,4 +1,7 @@
 from tkinter import *
+from tkinter.font import *
+
+
 
 window = Tk()
 window.title("Calculator")
@@ -19,21 +22,26 @@ def hello():
 
 
 
-Button(window, text=" 7 ", height=3,bg="gray46",width=6,fg="black").place(x=10,y=253)
-Button(window, text=" 8 ", height=3,bg="gray46",width=7,fg="black").place(x=80,y=253)
-Button(window, text=" 9 ", height=3,bg="gray46",width=7,fg="black").place(x=160,y=253)
-Button(window, text=" + ", height=3,width=6,bg="gray46",fg="black").place(x=240,y=253)
-Button(window, text=" 4 ", height=3,bg="gray46",width=6,fg="black").place(x=10,y=310)
-Button(window, text=" 5 ", height=3,bg="gray46",width=7,fg="black").place(x=80,y=310)
-Button(window, text=" 6 ", height=3,bg="gray46",width=7,fg="black").place(x=160,y=310)
-Button(window, text=" - ", height=3,width=6,bg="gray46",fg="black").place(x=240,y=310)
-Button(window, text=" 1 ", height=3,width=6,bg="gray46",fg="black").place(x=10,y=367)
-Button(window, text=" 2 ", height=3,width=7,bg="gray46",fg="black").place(x=80,y=367)
-Button(window, text=" 3 ", height=3,width=7,bg="gray46",fg="black").place(x=160,y=367)
-Button(window, text=" 0 ", height=2,width=6,bg="gray46",fg="black").place(x=10,y=424)
-Button(window, text=" . ", height=2,width=7,bg="gray46",fg="black").place(x=80,y=424)
-Button(window, text=" del ", height=2,width=7,bg="red",fg="black").place(x=160,y=424)
-Button(window, text=" * ", height=3,width=6,bg="gray46",fg="black").place(x=240,y=369)
-Button(window, text=" = ", height=2,width=6,bg="yellow",fg="black").place(x=240,y=424)
+myfont = Font(family="verdana",size="12")
+
+Button(window, text=" √ ", height=3,bg="#668cff",width=6,fg="black",font=myfont).place(x=10,y=196)
+Button(window, text=" <- ", height=3,bg="#668cff",width=7,fg="black",font=myfont).place(x=80,y=196)
+Button(window, text=" / ", height=3,bg="#668cff",width=7,fg="black",font=myfont).place(x=160,y=196)
+Button(window, text=" × ", height=3,bg="#668cff",width=6,fg="black",font=myfont).place(x=240,y=196)
+Button(window, text=" 7 ", height=3,bg="gray46",width=6,fg="black",font=myfont).place(x=10,y=253)
+Button(window, text=" 8 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=80,y=253)
+Button(window, text=" 9 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=160,y=253)
+Button(window, text=" + ", height=3,width=6,bg="#668cff",fg="black",font=myfont).place(x=240,y=253)
+Button(window, text=" 4 ", height=3,bg="gray46",width=6,fg="black",font=myfont).place(x=10,y=310)
+Button(window, text=" 5 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=80,y=310)
+Button(window, text=" 6 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=160,y=310)
+Button(window, text=" - ", height=3,width=6,bg="#668cff",fg="black",font=myfont).place(x=240,y=310)
+Button(window, text=" 1 ", height=3,width=6,bg="gray46",fg="black",font=myfont).place(x=10,y=367)
+Button(window, text=" 2 ", height=3,width=7,bg="gray46",fg="black",font=myfont).place(x=80,y=367)
+Button(window, text=" 3 ", height=3,width=7,bg="gray46",fg="black",font=myfont).place(x=160,y=367)
+Button(window, text=" 0 ", height=2,width=6,bg="gray46",fg="black",font=myfont).place(x=10,y=424)
+Button(window, text=" . ", height=2,width=7,bg="gray46",fg="black",font=myfont).place(x=80,y=424)
+Button(window, text=" del ",height=2,width=7,bg="orange red",fg="black",font=myfont).place(x=160,y=424)
+Button(window, text=" = ", height=5,width=6,bg="gold",fg="black",font=myfont).place(x=240,y=369)
 
 mainloop()
