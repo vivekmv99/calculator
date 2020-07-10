8bf0397c28c7d12ee8d4a93232cc3d6fbe98ecc2
Vivek Mv <mvvivek99@gmail.com>
2020-07-10 14:53:44 +0530

    button click for numbers

--- calcu_code.py
+++ calcu_code.py
@@ -18,6 +18,12 @@ dis_entry.place(x=20,y=30)
 
 dis_result = Entry(window,width=8,font=myfont_screen)
 dis_result.place(x=20,y=105)
+
+
+def button_click(number):
+    dis_entry.insert(0,number)
+
+
 def hello():
     print("hai")
 
@@ -30,18 +36,18 @@ Button(window, text=" √ ", height=3,bg="#668cff",width=6,fg="black",font=myfon
 Button(window, text=" <- ", height=3,bg="#668cff",width=7,fg="black",font=myfont).place(x=80,y=196)
 Button(window, text=" / ", height=3,bg="#668cff",width=7,fg="black",font=myfont).place(x=160,y=196)
 Button(window, text=" × ", height=3,bg="#668cff",width=6,fg="black",font=myfont).place(x=240,y=196)
-Button(window, text=" 7 ", height=3,bg="gray46",width=6,fg="black",font=myfont).place(x=10,y=253)
-Button(window, text=" 8 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=80,y=253)
-Button(window, text=" 9 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=160,y=253)
+Button(window, text=" 7 ", height=3,bg="gray46",width=6,fg="black",font=myfont,command=lambda:button_click(7)).place(x=10,y=253)
+Button(window, text=" 8 ", height=3,bg="gray46",width=7,fg="black",font=myfont,command=lambda:button_click(8)).place(x=80,y=253)
+Button(window, text=" 9 ", height=3,bg="gray46",width=7,fg="black",font=myfont,command=lambda:button_click(9)).place(x=160,y=253)
 Button(window, text=" + ", height=3,width=6,bg="#668cff",fg="black",font=myfont).place(x=240,y=253)
-Button(window, text=" 4 ", height=3,bg="gray46",width=6,fg="black",font=myfont).place(x=10,y=310)
-Button(window, text=" 5 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=80,y=310)
-Button(window, text=" 6 ", height=3,bg="gray46",width=7,fg="black",font=myfont).place(x=160,y=310)
+Button(window, text=" 4 ", height=3,bg="gray46",width=6,fg="black",font=myfont,command=lambda:button_click(4)).place(x=10,y=310)
+Button(window, text=" 5 ", height=3,bg="gray46",width=7,fg="black",font=myfont,command=lambda:button_click(5)).place(x=80,y=310)
+Button(window, text=" 6 ", height=3,bg="gray46",width=7,fg="black",font=myfont,command=lambda:button_click(6)).place(x=160,y=310)
 Button(window, text=" - ", height=3,width=6,bg="#668cff",fg="black",font=myfont).place(x=240,y=310)
-Button(window, text=" 1 ", height=3,width=6,bg="gray46",fg="black",font=myfont).place(x=10,y=367)
-Button(window, text=" 2 ", height=3,width=7,bg="gray46",fg="black",font=myfont).place(x=80,y=367)
-Button(window, text=" 3 ", height=3,width=7,bg="gray46",fg="black",font=myfont).place(x=160,y=367)
-Button(window, text=" 0 ", height=2,width=6,bg="gray46",fg="black",font=myfont).place(x=10,y=424)
+Button(window, text=" 1 ", height=3,width=6,bg="gray46",fg="black",font=myfont,command=lambda:button_click(1)).place(x=10,y=367)
+Button(window, text=" 2 ", height=3,width=7,bg="gray46",fg="black",font=myfont,command=lambda:button_click(2)).place(x=80,y=367)
+Button(window, text=" 3 ", height=3,width=7,bg="gray46",fg="black",font=myfont,command=lambda:button_click(3)).place(x=160,y=367)
+Button(window, text=" 0 ", height=2,width=6,bg="gray46",fg="black",font=myfont,command=lambda:button_click(0)).place(x=10,y=424)
 Button(window, text=" . ", height=2,width=7,bg="gray46",fg="black",font=myfont).place(x=80,y=424)
 Button(window, text=" del ",height=2,width=7,bg="orange red",fg="black",font=myfont).place(x=160,y=424)
 Button(window, text=" = ", height=5,width=6,bg="gold",fg="black",font=myfont).place(x=240,y=369)
